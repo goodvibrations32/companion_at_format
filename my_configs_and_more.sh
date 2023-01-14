50246bee382ef33f3f24a432cec6155981498a21
goodvibrations32 <goodvibrations32@protonmail.com>
2023-01-14 18:04:12 +0200

    checkout to local first then copy(for last changes)

--- my_configs_and_more.sh
+++ my_configs_and_more.sh
@@ -2,8 +2,9 @@
 
 #Add my repo to a folder in the new installation tree
 mkdir ~/my-settings
-git clone --depth 1 https://gitlab.com/goodvibrations32/dtos_dot_experiment.git ~/my-settings
+git clone https://gitlab.com/goodvibrations32/dtos_dot_experiment.git ~/my-settings
 cd ~/my-settings
+git checkout local
 cp -r .config/ ~/
 cp -r .xmonad/ ~/
 cp -r .doom.d/ ~/
