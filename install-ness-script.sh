3454d78d9bdc063aa1f572ed054b0a6c6ce2e0de
goodvibrations32 <goodvibrations32@protonmail.com>
2023-01-14 15:05:25 +0200

    adding some software and doom emacs

--- install-ness-script.sh
+++ install-ness-script.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
+# Update the needed iso after clean install
 sudo pacman -Syyu --noconfirm --needed
+
+# Download softwear and deps
+sudo pacman -S --noconfirm --needed alacritty
+sudo pacman -S --noconfirm --needed alacritty-themes
+sudo pacman -S --noconfirm --needed nitrogen
+sudo pacman -S --noconfirm --needed rofi
+sudo pacman -S --noconfirm --needed discord
+sudo pacman -S --noconfirm --needed firefox
+sudo pacman -S --noconfirm --needed signal-desktop
+sudo pacman -S --noconfirm --needed xmonad
+sudo pacman -S --noconfirm --needed emacs
+sudo pacman -S --noconfirm --needed ripgrep
+sudo pacman -S --noconfirm --needed fd
+sudo pacman -S --noconfirm --needed gnuplot
+sudo pacman -S --noconfirm --needed xmobar
+
+# sudo pacman -S --noconfirm --needed
+
+# Making the doom magic with my setup
+
+git clone --depth 1 https://github.com/doomemacs ~/.emacs.d
+~/.emacs.d/bin/doom install
