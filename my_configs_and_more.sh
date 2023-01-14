7b9249c1b02ab48ab3973d8f6fc84aa9aa4fd0bf
goodvibrations32 <goodvibrations32@protonmail.com>
2023-01-14 16:02:03 +0200

    fix path formation by cding to the dir

--- my_configs_and_more.sh
+++ my_configs_and_more.sh
@@ -3,6 +3,7 @@
 #Add my repo to a folder in the new installation tree
 mkdir ~/my-settings
 git clone --depth 1 https://gitlab.com/goodvibrations32/dtos_dot_experiment.git ~/my-settings
+cd ~/my-settings
 git checkout local
 cp -r .config/ ~/.config
 cp -r .xmonad/ ~/.xmonad
