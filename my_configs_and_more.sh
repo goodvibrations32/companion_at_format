19fedc6039d9ab88ee25701fd922d022c8faa408
goodvibrations32 <goodvibrations32@protonmail.com>
2023-01-14 15:45:28 +0200

    depth added for my repo (malfunc)

--- my_configs_and_more.sh
+++ my_configs_and_more.sh
@@ -2,7 +2,7 @@
 
 #Add my repo to a folder in the new installation tree
 mkdir ~/my-settings
-git clone https://gitlab.com/goodvibrations32/dtos_dot_experiment.git ~/my-settings
+git clone --depth 1 https://gitlab.com/goodvibrations32/dtos_dot_experiment.git ~/my-settings
 git checkout local
 cp -r .config/ ~/.config
 cp -r .xmonad/ ~/.xmonad
